c68e14795cfd6f6ad7f78e62f1e12756d3374503
rohanfaldu <159512577+Vinish14@users.noreply.github.com>
2025-01-09 10:56:22 +0530

    udpate variable

--- components/common/variable.js
+++ components/common/variable.js
@@ -1,6 +1,7 @@
 const variablesList = {
     currentPage: 1, 
     itemsPerPage: 10, 
+    // defaultLatlong: { lat: 33.5724032, lng: -7.6693941 },
     defaultLatlong: { lat: 33.5724032, lng: -7.6693941 },
 }
 
