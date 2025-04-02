1bc7311d507504155b1a900f4b2200099f18efe7
rohanfaldu <>
2025-04-02 21:14:04 +0530

    create build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/:all*(svg|jpg|png)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable'
+                    }
+                ]
+            }
+        ]
+    }
+}
