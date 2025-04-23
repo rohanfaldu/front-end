fc5f445892700e9410a43d3aac02864bd2dbabb2
jeet <jeetrathodinitiontechmedia@gmail.com>
2025-04-23 23:12:49 +0530

    resolve issue

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,216 +2,161 @@
 import Layout from "@/components/layout/Layout"
 import Link from "next/link"
 import { useTranslation } from "react-i18next";
+import React, { useEffect, useState } from 'react';
+import { useParams, useRouter } from 'next/navigation';
+import { insertData, insertImageData } from "../../components/api/Axios/Helper";
+import Preloader from "@/components/elements/Preloader";
+import variablesList from "@/components/common/Variable";
+
 export default function Blog() {
-	const { t } = useTranslation();
-	// return (
-	// 	<>
+	const [loading, setLoading] = useState(true);
+	const { t, i18n } = useTranslation();
+	const [blogDetails, setBlogDetails] = useState([]);
+	const [pagination, setPagination] = useState({
+		totalCount: 0,
+		totalPages: 1,
+		currentPage: variablesList.currentPage,
+		itemsPerPage: variablesList.itemsPerPage,
+	});
+	// Fetch data on component mount
 
-	// 		<Layout headerStyle={1} footerStyle={1} breadcrumbTitle={t("blog")}>
-	// 			<section className="flat-section">
-	// 				<div className="container">
-	// 					<div className="row">
-	// 						<div className="col-lg-8">
-	// 							<div className="flat-blog-list">
-	// 								<div className="flat-blog-item">
-	// 									<div className="img-style">
-	// 										<img src="/images/blog/blog-lg-1.jpg" alt="img-blog" />
-	// 										<span className="date-post">{t("date1")}</span>
-	// 									</div>
-	// 									<div className="content-box">
-	// 										<div className="post-author">
-	// 											<span className="text-black fw-7">Esther</span>
-	// 											<span>{t("furniture")}</span>
-	// 										</div>
-	// 										<h5 className="title">{t("title")}</h5>
-	// 										<p className="description body-1">{t("decs")}</p>
-	// 										<Link href="/blog-detail" className="btn-read-more">Read More</Link>
-	// 									</div>
-	// 								</div>
-	// 								<div className="flat-blog-item">
-	// 									<div className="img-style">
-	// 										<img src="/images/blog/blog-lg-2.jpg" alt="img-blog" />
-	// 										<span className="date-post">January 28, 2024</span>
-	// 									</div>
-	// 									<div className="content-box">
-	// 										<div className="post-author">
-	// 											<span className="text-black fw-7">Angel</span>
-	// 											<span>Interior</span>
-	// 										</div>
-	// 										<h5 className="title">92% of millennial homebuyers say inflation has impacted their plans</h5>
-	// 										<p className="description body-1">Delve into the art of home staging as an effective strategy to attract buyers and sell your home...</p>
-	// 										<Link href="/blog-detail" className="btn-read-more">Read More</Link>
-	// 									</div>
-	// 								</div>
-	// 								<div className="flat-blog-item">
-	// 									<div className="img-style">
-	// 										<img src="/images/blog/blog-lg-3.jpg" alt="img-blog" />
-	// 										<span className="date-post">January 28, 2024</span>
-	// 									</div>
-	// 									<div className="content-box">
-	// 										<div className="post-author">
-	// 											<span className="text-black fw-7">Eduardo</span>
-	// 											<span>Realtor</span>
-	// 										</div>
-	// 										<h5 className="title">The Art of Staging: How to Sell Your Home Quickly at a High Price.</h5>
-	// 										<p className="description body-1">Delve into the art of home staging as an effective strategy to attract buyers and sell your home...</p>
-	// 										<Link href="/blog-detail" className="btn-read-more">Read More</Link>
-	// 									</div>
-	// 								</div>
-	// 								<ul className="flat-pagination">
-	// 									<li><Link href="#" className="page-numbers current">1</Link></li>
-	// 									<li><Link href="#" className="page-numbers">2</Link></li>
-	// 									<li><Link href="#" className="page-numbers">3</Link></li>
-	// 									<li><Link href="#" className="page-numbers"><i className="icon icon-arr-r" /></Link></li>
-	// 								</ul>
-	// 							</div>
-	// 						</div>
-	// 						<div className="col-lg-4">
-	// 							<aside className="sidebar-blog fixed-sidebar">
-	// 								<div className="widget-search">
-	// 									<div className="h7 fw-7 text-black">{t("search")}</div>
-	// 									<div className="search-box">
-	// 										<input className="search-field" type="text" placeholder={t("search")}/>
-	// 										<Link href="#" className="right-icon icon-search" />
-	// 									</div>
-	// 								</div>
-	// 								<div className="widget-box bg-surface recent">
-	// 									<div className="h7 fw-7 text-black">{t("recentposts")}</div>
-	// 									<ul>
-	// 										<li>
-	// 											<Link href="/blog-detail" className="recent-post-item hover-img">
-	// 												<div className="img-style">
-	// 													<img src="/images/blog/post-recent-1.jpg" alt="post-recent" />
-	// 												</div>
-	// 												<div className="content">
-	// 													<span className="subtitle">{t("date")}</span>
-	// 													<div className="title">{t("post")}</div>
-	// 												</div>
-	// 											</Link>
-	// 										</li>
-	// 										<li>
-	// 											<Link href="/blog-detail" className="recent-post-item hover-img">
-	// 												<div className="img-style">
-	// 													<img src="/images/blog/post-recent-2.jpg" alt="post-recent" />
-	// 												</div>
-	// 												<div className="content">
-	// 													<span className="subtitle">{t("date")}</span>
-	// 													<div className="title">{t("post1")}</div>
-	// 												</div>
-	// 											</Link>
-	// 										</li>
-	// 										<li>
-	// 											<Link href="/blog-detail" className="recent-post-item hover-img">
-	// 												<div className="img-style">
-	// 													<img src="/images/blog/post-recent-3.jpg" alt="post-recent" />
-	// 												</div>
-	// 												<div className="content">
-	// 													<span className="subtitle">{t("date")}</span>
-	// 													<div className="title">{t("post2")}</div>
-	// 												</div>
-	// 											</Link>
-	// 										</li>
-	// 									</ul>
-	// 								</div>
-	// 								<div className="widget-box bg-surface categories">
-	// 									<div className="h7 fw-7 text-black">{t("bycategories")}</div>
-	// 									<ul>
-	// 										<li><Link href="#" className="categories-item"><span>{t("marketupdates")}</span><span>(112)</span></Link></li>
-	// 										<li><Link href="#" className="categories-item"><span>{t("buyingtips")}</span><span>(13)</span></Link></li>
-	// 										<li><Link href="#" className="categories-item current"><span>{t("interiorinspiration")}</span><span>(42)</span></Link></li>
-	// 										<li><Link href="#" className="categories-item"><span>{t("investmentinsights")}</span><span>(32)</span></Link></li>
-	// 										<li><Link href="#" className="categories-item"><span>{t("legalguidance")}</span><span>(54)</span></Link></li>
-	// 										<li><Link href="#" className="categories-item"><span>{t("homeconstruction")}</span><span>(93)</span></Link></li>
-	// 										<li><Link href="#" className="categories-item"><span>{t("communityspotlight")}</span><span>(52)</span></Link></li>
-	// 										<li><Link href="#" className="categories-item"><span>{t("trendwatch")}</span><span>(14)</span></Link></li>
-	// 									</ul>
-	// 								</div>
-	// 								<div className="widget-box bg-surface tag">
-	// 									<div className="h7 fw-7 text-black">{t("populartags")}</div>
-	// 									<ul>
-	// 										<li><Link href="#" className="tag-item">{t("property")}</Link></li>
-	// 										<li><Link href="#" className="tag-item current">{t("realty")}</Link></li>
-	// 										<li><Link href="#" className="tag-item">{t("finance")}</Link></li>
-	// 										<li><Link href="#" className="tag-item">{t("design")}</Link></li>
-	// 										<li><Link href="#" className="tag-item">{t("invest")}</Link></li>
-	// 										<li><Link href="#" className="tag-item">{t("legal")}</Link></li>
-	// 										<li><Link href="#" className="tag-item">{t("renovate")}</Link></li>
-	// 										<li><Link href="#" className="tag-item">{t("neighborhood")}</Link></li>
-	// 										<li><Link href="#" className="tag-item">{t("market")}</Link></li>
-	// 									</ul>
-	// 								</div>
-	// 							</aside>
-	// 						</div>
-	// 					</div>
-	// 				</div>
-	// 			</section>
+	const fetchBlogDetails = async (page) => {
+		try {
+			setLoading(true);
+			const lang = i18n.language;
+			const response = await insertData(`api/blog/getall`, { page, lang, limit: pagination.itemsPerPage }, false);
+	
+			if (response.status) {
+				const { totalCount, totalPages, blogs } = response.data; // REMOVE currentPage here
+				setBlogDetails(blogs);
+				setPagination((prev) => ({
+					...prev,
+					totalCount,
+					totalPages,
+				}));
+			} else {
+				console.log('Blog API failed');
+				router.push("/");
+			}
+		} catch (error) {
+			console.log('Error fetching blog:', error);
+		} finally {
+			setLoading(false);
+		}
+	};
+	
+	useEffect(() => {
+		if (i18n.isInitialized) {
+			fetchBlogDetails(pagination.currentPage);
+		}
+	}, [pagination.currentPage, i18n.language]);
 
-	// 		</Layout>
-	// 	</>
-	// )
+	const handlePageChange = (page) => {
+		console.log(page, '>>>>>> page');
+		setPagination((prev) => ({ ...prev, currentPage: page }));
+		//fetchBlogDetails(page);
+		// handleSubmit(page)
+	};
 
+	const formatDate = (dateString) => {
+		const date = new Date(dateString);
+		return date.toLocaleDateString("en-US", {
+			day: "2-digit",
+			month: "short",
+			year: "numeric",
+		});
+	}
+	console.log(blogDetails, '>>>>>>>> Blog')
+	console.log(pagination, '>>>>>>>> pagination')
 	return (
 		<>
-		<Layout headerStyle={1} footerStyle={1}>
-			<section className="flat-section-v3 flat-latest-new bg-surface">
-				<div className="container">
-					<div className="box-title text-center wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
-						<div className="text-subtitle text-primary">{t("latestnews")}</div>
-						<h4 className="mt-4">{t("guides")}</h4>
-					</div>
-					<div className="row">
-						<div className="box col-lg-4 col-md-6">
-							<Link href="/blog-detail" className="flat-blog-item hover-img wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
-								<div className="img-style">
-									<img src="/images/blog/blog-1.jpg" alt="img-blog" />
-									<span className="date-post">{t("date")}</span>
-								</div>
-								<div className="content-box">
-									<div className="post-author">
-										<span className="fw-6">{t("esther")}</span>
-										<span>{t("furniture")}</span>
+			{
+				loading ?
+					<Preloader />
+					: (
+						<>
+							<Layout headerStyle={1} footerStyle={1}>
+								<section className="flat-section-v3 flat-latest-new bg-surface blog-sec">
+									<div className="container">
+										<div className="box-title text-center wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
+											<div className="text-subtitle text-primary">{t("latestnews")}</div>
+											<h4 className="mt-4">{t("guides")}</h4>
+										</div>
+										<div className="row">
+											{
+												blogDetails.map((blogData) => (
+													<div className="box col-lg-4 col-md-6">
+														<Link href={`/blog/${blogData.slug}`} className="flat-blog-item hover-img wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
+															<div className="img-style">
+																<img src={blogData.image} alt="img-blog" className="blog-listing-img" />
+																<span className="date-post">{formatDate(blogData.created_at)}</span>
+															</div>
+															<div className="content-box">
+																<div className="post-author">
+																	{/* <span className="fw-6">{t("esther")}</span> */}
+																	{/* <span>{t("furniture")}</span> */}
+																</div>
+																<h6 className="title">{blogData.title}</h6>
+																{/* <p className="description">{t("decs")}</p> */}
+															</div>
+														</Link>
+													</div>
+												))
+											}
+
+											{/* <div className="box col-lg-4 col-md-6">
+												<Link href="/blog-detail" className="flat-blog-item hover-img wow fadeIn" data-wow-delay=".4s" data-wow-duration="2000ms">
+													<div className="img-style">
+														<img src="/images/blog/blog-2.jpg" alt="img-blog" />
+														<span className="date-post">{t("date1")}</span>
+													</div>
+													<div className="content-box">
+														<div className="post-author">
+															<span className="fw-6">{t("angle")}</span>
+															<span>{t("interior")}</span>
+														</div>
+														<h6 className="title">{t("maindecs1")}</h6>
+														<p className="description">{t("decs1")}</p>
+													</div>
+												</Link>
+											</div>
+											<div className="box col-lg-4 col-md-6">
+												<Link href="/blog-detail" className="flat-blog-item hover-img wow fadeIn" data-wow-delay=".6s" data-wow-duration="2000ms">
+													<div className="img-style">
+														<img src="/images/blog/blog-3.jpg" alt="img-blog" />
+														<span className="date-post">{t("date2")}</span>
+													</div>
+													<div className="content-box">
+														<div className="post-author">
+															<span className="fw-6">{t("colleen")}</span>
+															<span>{t("Architectur")}</span>
+														</div>
+														<h6 className="title">{t("maindecs2")}</h6>
+														<p className="description">{t("decs1")}</p>
+													</div>
+												</Link>
+											</div> */}
+										</div>
+										<ul className="wd-navigation">
+											{console.log(pagination.totalPages)}
+											{Array.from({ length: pagination.totalPages }, (_, index) => (
+												<li key={index}>
+													<Link
+														href="#" data-id={index}
+														className={`nav-item ${pagination.currentPage === index + 1 ? 'active' : ''}`}
+														onClick={() => handlePageChange(index + 1)}
+													>
+														{index + 1}
+													</Link>
+												</li>
+											))}
+										</ul>
 									</div>
-									<h6 className="title">{t("maindecs")}</h6>
-									<p className="description">{t("decs")}</p>
-								</div>
-							</Link>
-						</div>
-						<div className="box col-lg-4 col-md-6">
-							<Link href="/blog-detail" className="flat-blog-item hover-img wow fadeIn" data-wow-delay=".4s" data-wow-duration="2000ms">
-								<div className="img-style">
-									<img src="/images/blog/blog-2.jpg" alt="img-blog" />
-									<span className="date-post">{t("date1")}</span>
-								</div>
-								<div className="content-box">
-									<div className="post-author">
-										<span className="fw-6">{t("angle")}</span>
-										<span>{t("interior")}</span>
-									</div>
-									<h6 className="title">{t("maindecs1")}</h6>
-									<p className="description">{t("decs1")}</p>
-								</div>
-							</Link>
-						</div>
-						<div className="box col-lg-4 col-md-6">
-							<Link href="/blog-detail" className="flat-blog-item hover-img wow fadeIn" data-wow-delay=".6s" data-wow-duration="2000ms">
-								<div className="img-style">
-									<img src="/images/blog/blog-3.jpg" alt="img-blog" />
-									<span className="date-post">{t("date2")}</span>
-								</div>
-								<div className="content-box">
-									<div className="post-author">
-										<span className="fw-6">{t("colleen")}</span>
-										<span>{t("Architectur")}</span>
-									</div>
-									<h6 className="title">{t("maindecs2")}</h6>
-									<p className="description">{t("decs1")}</p>
-								</div>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</section>
-		</Layout>
+								</section>
+							</Layout>
+						</>
+					)
+			}
 		</>
 	)
 }
--- components/sections/Testimonial1.js
+++ components/sections/Testimonial1.js
@@ -3,6 +3,10 @@
 import { Autoplay, Navigation, Pagination } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
 import { useTranslation } from "react-i18next";
+import React, { useEffect, useState } from 'react';
+import { insertData, insertImageData } from "../../components/api/Axios/Helper";
+import Preloader from "@/components/elements/Preloader";
+import variablesList from "@/components/common/Variable";
 
 const swiperOptions = {
 	modules: [Autoplay, Pagination, Navigation],
@@ -34,7 +38,49 @@ const swiperOptions = {
 	},
 }
 export default function Testimonial1() {
-	const { t } = useTranslation();
+	const [loading, setLoading] = useState(true);
+	const { t, i18n } = useTranslation();
+	const [commentDetails, setCommentDetails] = useState([]);
+	const [pagination, setPagination] = useState({
+		totalCount: 0,
+		totalPages: 1,
+		currentPage: variablesList.currentPage,
+		itemsPerPage: variablesList.itemsPerPage,
+	});
+	// Fetch data on component mount
+
+	const fetchBlogDetails = async (page) => {
+		try {
+			setLoading(true);
+			const lang = i18n.language;
+			const response = await insertData(`api/property/get-all-comment`, { page, lang, limit: pagination.itemsPerPage }, false);
+
+			if (response.status) {
+				const { totalCount, totalPages, list } = response.data; // REMOVE currentPage here
+				setCommentDetails(list);
+				setPagination((prev) => ({
+					...prev,
+					totalCount,
+					totalPages,
+				}));
+			} else {
+				console.log('Blog API failed');
+				router.push("/");
+			}
+		} catch (error) {
+			console.log('Error fetching blog:', error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		if (i18n.isInitialized) {
+			fetchBlogDetails(pagination.currentPage);
+		}
+	}, [pagination.currentPage, i18n.language]);
+
+	console.log(commentDetails, ' >>>>> commentDetails')
 	return (
 		<>
 
@@ -55,99 +101,32 @@ export default function Testimonial1() {
 						<div className="col-lg-9">
 							<div className="swiper tf-sw-testimonial">
 								<Swiper {...swiperOptions} className="swiper-wrapper">
-									<SwiperSlide>
-										<div className="box-tes-item wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
-											<ul className="list-star">
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-											</ul>
-											<p className="note body-1">
-											{t("rating")}
-											</p>
-											<div className="box-avt d-flex align-items-center gap-12">
-												<div className="avatar avt-60 round">
-													<img src="/images/avatar/avt-7.jpg" alt="avatar" />
+									{commentDetails.length > 0
+										? commentDetails.map((commentData, index) => (
+											<SwiperSlide key={index}>
+												<div className="box-tes-item wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
+													<ul className="list-star">
+														{Array.from({ length: commentData.rating }, (_, starIndex) => (
+															<li key={starIndex} className="icon icon-star" />
+														))}
+													</ul>
+													<p className="note body-1">
+														{commentData.comment}
+													</p>
+													<div className="box-avt d-flex align-items-center gap-12">
+														<div className="avatar avt-60 round">
+															<img src={commentData.users?.image} alt="avatar" />
+														</div>
+														<div className="info">
+															<div className="h7 fw-7">{commentData.users?.full_name}</div>
+														</div>
+													</div>
 												</div>
-												<div className="info">
-													<div className="h7 fw-7">Liam Anderson</div>
-													<p className="text-variant-1 mt-4">	{t("ceodigital")}</p>
-												</div>
-											</div>
-										</div>
-									</SwiperSlide>
-									<SwiperSlide>
-										<div className="box-tes-item wow fadeIn" data-wow-delay=".4s" data-wow-duration="2000ms">
-											<ul className="list-star">
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-											</ul>
-											<p className="note body-1">
-											{t("rating1")}
-											</p>
-											<div className="box-avt d-flex align-items-center gap-12">
-												<div className="avatar avt-60 round">
-													<img src="/images/avatar/avt-5.jpg" alt="avatar" />
-												</div>
-												<div className="info">
-													<div className="h7 fw-7">Adam Will</div>
-													<p className="text-variant-1 mt-4">{t("ceoagency")}</p>
-												</div>
-											</div>
-										</div>
-									</SwiperSlide>
-									<SwiperSlide>
-										<div className="box-tes-item wow fadeIn" data-wow-delay=".6s" data-wow-duration="2000ms">
-											<ul className="list-star">
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-											</ul>
-											<p className="note body-1">
-												"My experience with property management services has exceeded expectations. They efficiently manage properties with a professional and attentive approach in every situation. I feel reassured that any issue will be resolved promptly and effectively."
-											</p>
-											<div className="box-avt d-flex align-items-center gap-12">
-												<div className="avatar avt-60 round">
-													<img src="/images/avatar/avt-5.jpg" alt="avatar" />
-												</div>
-												<div className="info">
-													<div className="h7 fw-7">Adam Will</div>
-													<p className="text-variant-1 mt-4">CEO Agency</p>
-												</div>
-											</div>
-										</div>
-									</SwiperSlide>
-									<div className="swiper-slide wow fadeIn" data-wow-delay=".6s" data-wow-duration="2000ms">
-										<div className="box-tes-item wow fadeIn" data-wow-delay=".2s" data-wow-duration="2000ms">
-											<ul className="list-star">
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-												<li className="icon icon-star" />
-											</ul>
-											<p className="note body-1">
-												"I truly appreciate the professionalism and in-depth knowledge of the brokerage team. They not only helped me find the perfect home but also assisted with legal and financial aspects, making me feel confident and secure in my decision."
-											</p>
-											<div className="box-avt d-flex align-items-center gap-12">
-												<div className="avatar avt-60 round">
-													<img src="/images/avatar/avt-7.jpg" alt="avatar" />
-												</div>
-												<div className="info">
-													<div className="h7 fw-7">Liam Anderson</div>
-													<p className="text-variant-1 mt-4">CEO Digital</p>
-												</div>
-											</div>
-										</div>
-									</div>
+											</SwiperSlide>
+										))
+										: null}
 								</Swiper>
+
 							</div>
 						</div>
 					</div>
